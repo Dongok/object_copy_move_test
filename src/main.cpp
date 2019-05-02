8819d6f13e3c0f84d9aafa6e29476e5d600c4f40
dongok.lee <dongok.lee.81@gmail.com>
2019-05-03 00:22:54 +0900

    malloc 테스트 포함...중.. 어렵네요..

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,7 @@
 int number_of_allocs = 0;
 
 void *operator new(std::size_t size) throw(std::bad_alloc) {
+  std::cout << "malloc new1\n";
   ++number_of_allocs;
   void *p = malloc(size);
   if (!p) throw std::bad_alloc();
@@ -18,6 +19,7 @@ void *operator new(std::size_t size) throw(std::bad_alloc) {
 }
 
 void *operator new[](std::size_t size) throw(std::bad_alloc) {
+  std::cout << "malloc new2\n";
   ++number_of_allocs;
   void *p = malloc(size);
   if (!p) throw std::bad_alloc();
@@ -25,10 +27,12 @@ void *operator new[](std::size_t size) throw(std::bad_alloc) {
 }
 
 void *operator new[](std::size_t size, const std::nothrow_t &) throw() {
+  std::cout << "malloc new3\n";
   ++number_of_allocs;
   return malloc(size);
 }
 void *operator new(std::size_t size, const std::nothrow_t &) throw() {
+  std::cout << "malloc new4\n";
   ++number_of_allocs;
   return malloc(size);
 }
